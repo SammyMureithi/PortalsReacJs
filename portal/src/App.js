c83c74fe86f0298ef6a2317c2cb2f3fefe154f85
Sammy Mureithi <sammymureithi20@gmail.com>
2022-08-26 03:16:31 +0300

    Portals

---
+++ portal/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PortalDemo from './PortalDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <PortalDemo/>
+    </div>
+  );
+}
+
+export default App;
